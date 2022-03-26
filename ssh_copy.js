130c9c052b652514e4aa08508fb49d8e5f17df43
Jasen Green <jasen.green@protonmail.com>
2022-03-26 09:33:30 -0700

    Updated to Nodejs 17
    Update ssh client and fs.writeFileSync option variable

--- ssh_copy.js
+++ ssh_copy.js
@@ -1,10 +1,8 @@
 let ip = {
-    url: "http://whatismyip.akamai.com/"
+  url: "http://whatismyip.akamai.com/",
 };
 
-let {
-    exec
-} = require("child_process");
+let { exec } = require("child_process");
 
 let fs = require("fs");
 let figlet = require("figlet");
@@ -21,24 +19,21 @@ let SftpClient = require("ssh2-sftp-client");
 let remoteFile;
 let jobcount = 0;
 let sftp = new SftpClient();
-let client = new WebTorrent()
+let client = new WebTorrent();
 
-
-figlet('SSH Torrent Copy', function (err, data) {
-    if (err) {
-        console.log('Something went wrong...');
-        console.dir(err);
-        return;
-    };
-    console.log(data, "\n", "\n");
+figlet("SSH Torrent Copy", function (err, data) {
+  if (err) {
+    console.log("Something went wrong...");
+    console.dir(err);
+    return;
+  }
+  console.log(data, "\n", "\n");
 });
 
-
-process.argv[2]
+process.argv[2];
 
 if (process.argv[2] === "--help" || typeof process.argv[2] === "string") {
-
-    console.log(`\n ssh_copy downloads torrents and moves to folders of choice.
+  console.log(`\n ssh_copy downloads torrents and moves to folders of choice.
 
 usage:
   ssh_copy <command>
@@ -49,236 +44,219 @@ usage:
   Music: Moves data to the music folder.
   --help: Used to print the usage guide. \n`);
 
-    process.exit();
-
-};
-
-let questions = [{
-    type: 'input',
-    name: 'mag',
-    message: "Please add Magnet Url Seperated by a space:",
-},
-{
-    type: 'input',
-    name: 'folder',
-    message: "Inputs Folder Location:",
+  process.exit();
 }
+
+let questions = [
+  {
+    type: "input",
+    name: "mag",
+    message: "Please add Magnet Url Seperated by a space:",
+  },
+  {
+    type: "input",
+    name: "folder",
+    message: "Inputs Folder Location:",
+  },
 ];
 
 function whenDone(callback) {
+  setTimeout(function () {
+    inquirer.prompt(questions).then((answers) => {
+      if (answers.folder === "Movies") {
+        file = config.variables.localFilemovies;
+        remoteFile = config.variables.remoteFilemovies;
+      } else if (answers.folder === "Music") {
+        file = config.variables.localFilemusic;
+        remoteFile = config.variables.remoteFilemusic;
+      } else {
+        console.log(
+          "\n" +
+            "Inputs dont match acceptable data check --help for more information."
+              .red
+        );
+        process.exit();
+      }
 
-    setTimeout(function () {
+      let count = 0;
+      let text = answers.mag.split(" ");
 
-        inquirer.prompt(questions).then(answers => {
+      //Log to File Magnet Link
 
-            if (answers.folder === "Movies") {
+      //  fs.writeFileSync("/home/jasen/magnet.txt", text); Works in node12
 
-                file = config.variables.localFilemovies;
-                remoteFile = config.variables.remoteFilemovies;
+      fs.writeFileSync("/home/jasen/magnet.txt", JSON.stringify(text)); 
 
-            } else if (answers.folder === "Music") {
+      console.log(colors.yellow("\n" + text + "\n"));
+      //Start Webtorrent
+      for (let i = 0; i < text.length; i++) {
+        client.add(
+          text[i],
+          {
+            path: file,
+          },
+          function (torrent) {
+            let interval = setInterval(function () {
+              console.log(
+                "Progress: " + (client.progress * 100).toFixed(1) + "%".cyan
+              );
+            }, 5000);
 
-                file = config.variables.localFilemusic;
-                remoteFile = config.variables.remoteFilemusic;
-            } else {
+            torrent.on("done", function () {
+              count++;
 
-                console.log("\n" + "Inputs dont match acceptable data check --help for more information.".red);
-                process.exit();
-            };
+              console.log(`Torrent download ${count} completed... \n`.green);
 
-            let count = 0;
-            let text = answers.mag.split(" ");
+              if (count === text.length) {
+                let done = "done";
+                callback(done);
+              }
 
-            //Log to File Magnet Link
-            fs.writeFileSync("/home/jasen/magnet.txt", text);
-
-            console.log(colors.yellow("\n" + text + "\n"));
-            //Start Webtorrent
-            for (let i = 0; i < text.length; i++) {
-
-                client.add(text[i], {
-                    path: file
-                }, function (torrent) {
-
-                    let interval = setInterval(function () {
-                        console.log('Progress: ' + (client.progress * 100).toFixed(1) + '%'.cyan);
-                    }, 5000)
-
-                    torrent.on('done', function () {
-                        count++;
-
-                        console.log(`Torrent download ${count} completed... \n`.green)
-
-                        if (count === text.length) {
-
-                            let done = "done";
-                            callback(done)
-                        };
-
-                        clearInterval(interval)
-
-                    });
-                });
-            };
-
-        });
-    }, 1000)
-
-};
+              clearInterval(interval);
+            });
+          }
+        );
+      }
+    });
+  }, 1000);
+}
 
 whenDone(function (done) {
-    //Turn Off VPN
-    if (done) {
+  //Turn Off VPN
+  if (done) {
+    let vpnstop = exec("sh /home/jasen/ssh_copy/vpn_stop.sh");
 
-        let vpnstop = exec('sh /home/jasen/ssh_copy/vpn_stop.sh');
-
-        vpnstop.stdout.on('data', (data) => {
-            console.log(data);
-
-        });
-
-        mainJob.start();
-    }
-
-});
-
-
-let mainJob = new CronJob('*/3 * * * *', function () {
-
-    function tranferFile(callback) {
-        //Pull directory list
-        fs.readdir('/media/more_data/Movies', function (err, files) {
-            //Pull Directory List from Server
-            if (err) {
-                return console.log('Unable to scan directory: ' + err);
-            }
-
-            sftp.connect(config)
-                .then(() => {
-                    return sftp.list(remoteFile)
-                })
-                .then(data => {
-
-                    let found;
-                    let arr1 = [];
-
-                    for (let i = 0; i < data.length; i++) {
-
-                        found = files.find(movie => movie === data[i].name);
-                        arr1.push(found)
-
-                        if (typeof found === "string") {
-
-                            console.log(`Folder - "${found}" is already on Server`.red);
-                            found = 'DontRun';
-                            callback(null, found);
-
-                            break;
-                        };
-                    };
-                    //Search Server List Data 
-                    let createUndefinded = (values) => values === undefined;
-                    let arrValue = arr1.every(createUndefinded);
-
-                    if (arrValue) {
-                        found = 'Run';
-                        callback(null, found);
-                    };
-                    sftp.end();
-
-                }).catch(err => {
-                    console.log(`Error: ${err.message} \n`.red);
-
-                    curl.request(ip, function (err, data) {
-                        console.log(`Public Ip Address \n ${data} \n`.yellow);
-                    });
-                });
-
-        });
-    };
-
-    tranferFile(function (err, found) {
-
-        if (found === 'DontRun') {
-
-            //Check Current IP Addresss
-            let job1 = new CronJob('* * * * *', function () {
-                curl.request(ip, function (err, data) {
-                    console.log(`Public Ip Address \n ${data} \n`.yellow);
-                });
-            });
-
-            job1.start();
-
-        } else if (found === 'Run') {
-            //Count amount of time job has ran
-            jobcount++
-
-            console.log("Run Times:", colors.cyan(jobcount), "\n");
-
-            curl.request(ip, function (err, data) {
-
-                //Display Public IP address
-                console.log(`Public Ip Address \n ${data} \n`.yellow);
-
-                if (data === '98.#.#.#' && jobcount === 1) {
-
-                    //SSH Connection Upload to Server
-                    async function main() {
-
-                        let client = new SftpClient();
-                        const src = file;
-                        const dst = remoteFile;
-
-                        try {
-                            await client.connect(config)
-
-                            client.on('upload', info => {
-
-                                console.log(`Listener: Uploaded ${info.source} \n`.green);
-
-                                fs.appendFile('/home/jasen/ssh_copy/logs.txt', `${info.source} \n`, (err) => {
-                                    if (err) throw err;
-                                });
-                            });
-
-                            let rslt = await client.uploadDir(src, dst);
-                            return rslt;
-
-                        }
-
-                        catch (err) {
-                            console.log(err)
-
-                        } finally {
-
-                            client.end();
-                            sendmail();
-                            deleteFolder();
-                        };
-                    };
-
-                    main()
-                        .then(msg => {
-                            console.log(colors.green(msg, "\n"));
-
-                        })
-                        .catch(err => {
-                            console.log(`main error: ${err.message} \n`.red);
-
-                            curl.request(ip, function (err, data) {
-                                console.log(`Public Ip Address \n ${data} \n`.yellow);
-                            });
-                        });
-
-                } else {
-                    console.log("\n" + "No Files to Upload" + "\n".rainbow);
-                };
-            });
-
-        };
+    vpnstop.stdout.on("data", (data) => {
+      console.log(data);
     });
 
-
+    mainJob.start();
+  }
 });
 
+let mainJob = new CronJob("*/4 * * * *", function () {
+  function tranferFile(callback) {
+    //Pull directory list
+    fs.readdir("/media/more_data/Movies", function (err, files) {
+      //Pull Directory List from Server
+      if (err) {
+        return console.log("Unable to scan directory: " + err);
+      }
+
+      sftp
+        .connect(config)
+        .then(() => {
+          return sftp.list(remoteFile);
+        })
+        .then((data) => {
+          let found;
+          let arr1 = [];
+
+          for (let i = 0; i < data.length; i++) {
+            found = files.find((movie) => movie === data[i].name);
+            arr1.push(found);
+
+            if (typeof found === "string") {
+              console.log(`Folder - "${found}" is already on Server`.red);
+              found = "DontRun";
+              callback(null, found);
+
+              break;
+            }
+          }
+          //Search Server List Data
+          let createUndefinded = (values) => values === undefined;
+          let arrValue = arr1.every(createUndefinded);
+
+          if (arrValue) {
+            found = "Run";
+            callback(null, found);
+          }
+
+          sftp.end();
+        })
+        .catch((err) => {
+          console.log(`Error: ${err.message} \n`.red);
+
+          curl.request(ip, function (err, data) {
+            console.log(`Public Ip Address \n ${data} \n`.yellow);
+          });
+        });
+    });
+  }
+
+  tranferFile(function (err, found) {
+    if (found === "DontRun") {
+      //Check Current IP Addresss
+      let job1 = new CronJob("* * * * *", function () {
+        curl.request(ip, function (err, data) {
+          console.log(`Public Ip Address \n ${data} \n`.yellow);
+        });
+      });
+
+      job1.start();
+    } else if (found === "Run") {
+      //Count amount of time job has ran
+      jobcount++;
+
+      console.log("Run Times:", colors.cyan(jobcount), "\n");
+
+      curl.request(ip, function (err, data) {
+        //Display Public IP address
+        console.log(`Public Ip Address \n ${data} \n`.yellow);
+
+        if (data === "" && jobcount === 1) {
+          //SSH Connection Upload to Server
+          async function main() {
+            let client = new SftpClient();
+            const src = file;
+            const dst = remoteFile;
+
+            try {
+              await client.connect(config);
+
+              client.on("upload", (info) => {
+                console.log(`Listener: Uploaded ${info.source} \n`.green);
+
+                fs.appendFile(
+                  "/home/jasen/ssh_copy/logs.txt",
+                  `${info.source} \n`,
+                  (err) => {
+                    if (err) throw err;
+                  }
+                );
+              });
+
+              let rslt = await client.uploadDir(src, dst);
+              return rslt;
+            } catch (err) {
+              console.log(err);
+            } finally {
+              client.end();
+              sendmail();
+              deleteFolder();
+            }
+          }
+
+          main()
+            .then((msg) => {
+              console.log(colors.green(msg, "\n"));
+            })
+            .catch((err) => {
+              console.log(`main error: ${err.message} \n`.red);
+
+              curl.request(ip, function (err, data) {
+                console.log(`Public Ip Address \n ${data} \n`.yellow);
+              });
+            });
+        } else {
+          console.log("\n" + "No Files to Upload" + "\n".rainbow);
+          //Kill Program
+          if (jobcount === 5) {
+            process.exit(1);
+          }
+        }
+      });
+    }
+  });
+});
