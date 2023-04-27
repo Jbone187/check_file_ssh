e359b118c6319c0923e1aba041867962db04f44b
Jasen Green <jasen.green@protonmail.com>
2023-04-26 21:05:35 -0700

    Create package.js

---
+++ package.js
@@ -0,0 +1,21 @@
+{
+  "name": "ssh_copy",
+  "version": "1.0.0",
+  "description": "Copy Files",
+  "main": "ssh_copy.js",
+  "scripts": {
+    "test": "echo \"Error: no test specified\" && exit 1"
+  },
+  "author": "Jasen Green",
+  "license": "ISC",
+  "dependencies": {
+    "colors": "^1.4.0",
+    "cron": "^2.0.0",
+    "curlrequest": "^1.0.1",
+    "figlet": "^1.5.2",
+    "inquirer": "^8.2.5",
+    "nodemailer": "^6.7.3",
+    "ssh2-sftp-client": "^9.0.4",
+    "webtorrent": "^1.9.7"
+  }
+}
